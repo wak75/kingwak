96e82785987a625a2eb16af8c785e3f53b6b58aa
wak75 <61159573+wak75@users.noreply.github.com>
2020-02-17 23:55:30 +0530

    Create multiple inheritance.cpp
    This is a simple code to show multiple inheritances.

---
+++ multiple inheritance.cpp
@@ -0,0 +1,88 @@
+/*Design 3 classes, 1.student, 2.exam ,3.results.
+class student contain data members: ->Name, ->Roll No.
+class exam contain:-> marks of 6 subject.
+class results contain: ->data member which shows an average of all of your 6 subjects as well as your details.*/
+
+#include<iostream>
+using namespace std;
+
+class student
+{
+	int roll;
+	char name[10];
+	
+	public:
+	void getdata()
+	{
+		cout<<"Enter your name: "<<endl;
+		cin>>name;
+		cout<<"Enter your Roll no.:"<<endl;
+		cin>>roll;
+	}
+	void display()
+	{
+		cout<<"your name is: "<<name<<endl;
+		cout<<"your Roll no. is: "<<roll<<endl;
+	}
+};
+
+class exam: public student
+{
+	public:
+	int m1,m2,m3,m4,m5,m6;
+	
+	void getdata1()
+	{
+		cout<<"Enter your marks of subject 1: "<<endl;
+		cin>>m1;
+		cout<<"Enter your marks of subject 2: "<<endl;
+		cin>>m2;
+		cout<<"Enter your marks of subject 3: "<<endl;
+		cin>>m3;
+		cout<<"Enter your marks of subject 4: "<<endl;
+		cin>>m4;
+		cout<<"Enter your marks of subject 5: "<<endl;
+		cin>>m5;
+		cout<<"Enter your marks of subject 6: "<<endl;
+		cin>>m6;
+	}
+	void display1()
+	{
+		//void display();
+		cout<<"Marks for Subject 1 is: "<<m1<<endl;
+		cout<<"Marks for Subject 2 is: "<<m2<<endl;
+		cout<<"Marks for Subject 3 is: "<<m3<<endl;
+		cout<<"Marks for Subject 4 is: "<<m4<<endl;
+		cout<<"Marks for Subject 5 is: "<<m5<<endl;
+		cout<<"Marks for Subject 6 is: "<<m6<<endl;
+	
+	}
+};
+
+class results:public exam
+{
+	public:
+	
+	int c=(m1+m2+m3+m4+m5+m6)/6;	
+	void final()
+	{
+		//void display1();
+		cout<<"final average marks of 6 subjects are: "<<c<<endl;
+	}
+};
+
+int main()
+{
+	student ob1;
+	ob1.getdata();
+	
+	exam ob2;
+	ob2.getdata1();
+	
+	results ob3;
+	
+	
+	ob1.display();
+	ob2.display1();
+	ob3.final();
+}
