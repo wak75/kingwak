54f861fb811f43dc8619d956a503be822680dac6
Washim Akram Khan <61159573+wak75@users.noreply.github.com>
2020-04-08 14:25:29 +0530

    Add files via upload

---
+++ 19bca1141_p10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	ifstream f1;
+	f1.open("work.txt");
+	char ch[50];
+	int x=0,i;
+	while(!f1.eof())
+	{
+		f1.getline(ch,50,'.');
+		for(i=0;i<=50;i++)
+		{
+			if(ch[i]=='A')
+			{
+				x++;	
+			}	
+		}
+	}
+	cout<<"Total number of lines starting with 'A' are: "<<x;
+}
